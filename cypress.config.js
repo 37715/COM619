98da7fca91fccf58fce8aaec5af2d122239f9b65
MattMW04 <6wilcm63@solent.ac.uk>
2024-12-03 11:38:38 +0000

    Set up Code Monitoring when JS files are available to test

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here if needed
+      require('@cypress/code-coverage/task')(on, config);  
+      // return config variable
+      return config;   
     },
     baseUrl: 'http://localhost:8080', 
   },
